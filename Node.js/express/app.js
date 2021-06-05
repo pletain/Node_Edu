1104205d56aa75c55b66fb854f65f0abfe52e554
김태형 <thk12378905@gmail.com>
2021-06-05 07:14:12 +0000

    the end

--- Node.js/express/app.js
+++ Node.js/express/app.js
@@ -4,60 +4,88 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 
-const admin = require('./routes/admin');
-// const contacts = require('./routes/contacts');
+class App {
 
-const app = express(); //객체
-const port = 3000;
+    constructor () {
+        this.app = express();
+        
+        // 뷰엔진 셋팅
+        this.setViewEngine();
 
-nunjucks.configure('template' , { //template 폴더 이후 파일 위치를 admin.js에서 설정해주면 된다.
-	autoescape : true, //보안상 툴
-	express : app //객체를 설정
-});
+        // 미들웨어 셋팅
+        this.setMiddleWare();
 
-//미들웨어 셋팅
-app.use( logger('dev') );
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded({ extended : false }) );
+        // 정적 디렉토리 추가
+        this.setStatic();
 
-app.use('/uploads', express.static('uploads')); //use는 항상 앞에 URL이 나와주면 된다.
-//앞에가 URL 뒤에가 폴더명이다.
+        // 로컬 변수
+        this.setLocals();
 
-app.use( (req, res , next)  =>{
-	app.locals.isLogi
-});
+        // 라우팅
+        this.getRouting();
 
-//정적파일 서빙
+        // 404 페이지를 찾을수가 없음
+        this.status404();
 
-app.get('/', (req, res) => { //request를 받아서 response를 준다. 앞에는 주소를, 뒤쪽에는 function을 넣어주면 된다.
-	res.send('hello express');	
-});
+        // 에러처리
+        this.errorHandler();
 
-// app.get('/new', (req, res) => { //주소 추가 방법
-// 	res.send('newbie!!');
-// });
- 
-// function adminmiddleware(req, res, next) {
-// 	console.log('admin을 거치는 모든 곳에서 가장 먼저 실행된다.');
-// 	next();
-// }
 
-app.use('/admin', admin ); 
+    }
 
-// app.use('/contacts', contacts);
 
-app.use( (req,res,_) =>{
-	res.status(400).render('common/404.html');
-});
+    setMiddleWare (){
+        
+        // 미들웨어 셋팅
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use( (req,res,_) =>{
-	res.status(500).render('common/500.html');
-});
-	 
+    }
 
-app.listen(port, () => { //HTTP createServer격
-	console.log('Express listening on port', port)
-});
+    setViewEngine (){
 
-//node app.js -> nodemon app.js 코드를 변경할 때마다 수시로 서버를 끄고 저장하고 다시 서버를 키기 귀찮을 때
-//npm install -g nodemon
+        nunjucks.configure('template', {
+            autoescape: true,
+            express: this.app
+        });
+
+    }
+
+
+    setStatic (){
+        this.app.use('/uploads', express.static('uploads'));
+    }
+
+    setLocals(){
+
+        // 템플릿 변수
+        this.app.use( (req, res, next) => {
+            this.app.locals.isLogin = true;
+            this.app.locals.req_path = req.path;
+            next();
+        });
+
+    }
+
+    getRouting (){ //app 셋팅 이후 라우팅 설정
+        this.app.use(require('./controllers'))
+    }
+
+    status404() {  // error handling
+        this.app.use( ( req , res, _ ) => {
+            res.status(404).render('common/404.html')
+        });
+    }
+
+    errorHandler() {
+
+        this.app.use( (err, req, res,  _ ) => {
+            res.status(500).render('common/500.html')
+        });
+    
+    }
+
+}
+
+module.exports = new App().app; //파일을 호출하면 인스턴스가 생성되면서 뿌려짐.
